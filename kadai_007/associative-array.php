97c9d6a9bdc166d27271e452180d50c19b5d3439
pekozaru <160243447+pekozaru@users.noreply.github.com>
2024-08-30 15:11:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列に値を代入
+        $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 連想配列の値を出力
+        print_r($array);
+        ?>
+    </p>
+</body>
+
+</html>
